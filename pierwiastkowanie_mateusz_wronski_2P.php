5658272fd322d0111f9302b5d958f8efffe426a9
mattBr1ght <mateuszwronski.yt@gmail.com>
2024-04-17 21:51:50 +0200

    futher improvements

--- pierwiastkowanie_mateusz_wronski_2P.php
+++ pierwiastkowanie_mateusz_wronski_2P.php
@@ -1,6 +1,6 @@
 <?php
 $a = readline("Podaj liczbę do obliczenia pierwiastka : ");
-if (!ctype_digit($a)) {
+if (!is_numeric($a)) { //wcześniej !ctype_digit($a)
   echo "Nie podano poprawnej liczby";
   exit(0);
 }
@@ -12,9 +12,17 @@ if ($a <= 0) {
 function sqrt_iterative($a)
 {
   $buffer = -1;
+  $i = 1;
   $last_x = $a;
-  while ($buffer != $last_x) {
-    echo $last_x . " ";
+  $continue = null;
+  $max_k = 100;
+  while ($buffer != $last_x || ($i <= $max_k and $continue == true) || is_null($continue)) {
+    if(is_null($continue)){
+      $read = readline("Czy kontynuować liczenie pomimo powtarzalności ostatnich wyników (y/n)? : ");
+      $continue = ($read == "y" ? true : ($read == "n" ? false : null));
+      continue;
+    }
+    echo $i++ . ". " . $last_x . " \n";
     $buffer = $last_x;
     $last_x = (1 / 2) * ($last_x + ($a / $last_x));
   }
@@ -32,5 +40,5 @@ function sqrt_recursive($a, $k = 0)
   return (1 / 2) * ($res + ($a / $res));
 }
 
-echo "\npierwiastek z $a wynosi : " . sqrt_iterative($a);
+//echo "\npierwiastek z $a wynosi : " . sqrt_iterative($a);
 echo "\npierwiastek z $a wynosi : " . sqrt_recursive($a, 100);
